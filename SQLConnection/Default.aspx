ae678dba8ad0bc4b710333b52e5ff00385885ecd
Arturo Ibarra <arturo.ibarra.g95@gmail.com>
2021-05-14 15:51:12 -0500

    Cap 16 conexión con base de datos

---
+++ SQLConnection/Default.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SQLConnection._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        
+        <asp:GridView ID="gdvComestibles" runat="server" AutoGenerateColumns="true"></asp:GridView>
+
+        <asp:DropDownList ID="ddlComestibles" runat="server"></asp:DropDownList>
+
+    </div>
+
+</asp:Content>
---
+++ SQLConnection/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SQLConnection
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connDB"].ConnectionString))
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "SELECT * FROM comestibles";
+                    cmd.Connection = conn;
+                    conn.Open();
+                    var reader = cmd.ExecuteReader();
+                    gdvComestibles.DataSource = reader;
+                    gdvComestibles.DataBind();
+                    reader.Close();
+                    reader = cmd.ExecuteReader();
+                    ddlComestibles.DataSource = reader;
+                    ddlComestibles.DataValueField = "id";
+                    ddlComestibles.DataTextField = "nombre";
+                    ddlComestibles.DataBind();
+                }
+            }
+        }
+    }
+}
